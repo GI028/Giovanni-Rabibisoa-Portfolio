142d25b3512f4b8dc325f1385a7abbf34f470c02
Giovanni <187939419+GI028@users.noreply.github.com>
2025-03-05 00:43:21 +0800

    fixing skills showing page overflow on mobile

--- src/pages/Skills/component/SkillSection.tsx
+++ src/pages/Skills/component/SkillSection.tsx
@@ -12,7 +12,7 @@ const SkillSection = <T,>({
   gap = 4,
 }: SkillSectionProps<T>) => (
   <div>
-    <h1 className="text-text-2 mb-5 text-lg">{title}</h1>
+    <h1 className="text-text-2 mb-5 text-lg overflow-hidden">{title}</h1>
     <div className={`flex items-center w-full flex-wrap gap-${gap} px-2`}>
       {data.map((item, index) => renderItem(item, index))}
     </div>
